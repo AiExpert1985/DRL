ee8b6699b5ed0508e454b8b0c62d117ef3323b77
Mohamad Nawfal <aiexpert2025@gmail.com>
2022-06-23 14:19:43 +0300

    ...

--- DQN/gdrl_DQN.py
+++ DQN/gdrl_DQN.py
@@ -1,4 +1,7 @@
 import gym
+import torch.cuda
+from torch import nn
+import torch.nn.functional as F
 
 
 def get_make_env_fn(**kargs):
@@ -13,6 +16,49 @@ def get_make_env_fn(**kargs):
             env = gym.make(env_name)
         return env
     return make_env_fn, kargs
+
+
+class FCQ(nn.Module):
+    def __init__(self,
+                 input_dim,
+                 output_dim,
+                 hidden_dims=(32, 32),
+                 activation_fc=F.relu):
+        super(FCQ, self).__init__()
+        self.activation_fc = activation_fc
+        self.input_layer = nn.Linear(input_dim, hidden_dims[0])
+        self.hidden_layers = nn.ModuleList()
+        for i in range(len(hidden_dims) - 1):
+            hidden_layer = nn.Linear(hidden_dims[i], hidden_dims[i+1])
+            self.hidden_layers.append(hidden_layer)
+        self.output_layer = nn.Linear(hidden_dims[-1], output_dim)
+        device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        self.device = torch.device(device)
+        self.to(self.device)
+
+    def _format(self, state):
+        x = state
+        if not isinstance(x, torch.Tensor):
+            x = torch.tensor(x, device=self.device, dtype=torch.float32)
+            x = x.unsqueeze(0)
+        return x
+
+    def forward(self, state):
+        x = self._format(state)
+        x = self.activation_fc(self.input_layer(x))
+        for hidden_layer in self.hidden_layers:
+            x = self.activation_fc(hidden_layer(x))
+        x = self.output_layer(x)
+        return x
+
+    def load(self, experiences):
+        states, actions, rewards, new_states, is_terminals = experiences
+        states = torch.from_numpy(states).float().to(self.device)
+        actions = torch.from_numpy(actions).long().to(self.device)
+        new_states = torch.from_numpy(new_states).float().to(self.device)
+        rewards = torch.from_numpy(rewards).float().to(self.device)
+        is_terminals = torch.from_numpy(is_terminals).float().to(self.device)
+        return states, actions, rewards, new_states, is_terminals
 
 
 class DQN:

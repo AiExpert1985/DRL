cc0e55b27317319937a5da5da9ece52e3c2fecf5
Mohamad Nawfal <aiexpert2025@gmail.com>
2022-07-26 13:15:47 +0300

    ...

--- Book Sutton/chapter02/my_chapter02.py
+++ Book Sutton/chapter02/my_chapter02.py
@@ -4,8 +4,8 @@ from tqdm import tqdm
 
 
 class Bandit:
-    def __init__(self, arms=10, epsilon=0.0, lr=0.1, is_sample_avg=False, is_ucb=False,
-                 is_grad=False, is_optim=False, is_station=True):
+    def __init__(self, arms=10, epsilon=0.0, lr=0.1, q_true_initial=0., q_estimated_initial=0.,
+                 is_sample_avg=False, is_ucb=False, is_grad=False, is_stationary=True):
         self.arms = arms
         self.actions = np.arange(self.arms)
         self.epsilon = epsilon
@@ -13,37 +13,48 @@ class Bandit:
         self.is_sample_avg = is_sample_avg
         self.is_ucb = is_ucb
         self.is_grad = is_grad
-        self.is_optim = is_optim
-        self.is_station = is_station
+        self.is_stationary = is_stationary
+        self.q_true_initial = q_true_initial
+        self.q_estimated_initial = q_estimated_initial
 
     def reset(self):
-        self.t = 0
-        self.q_true = np.random.randn(self.arms)
-        self.q_estimated = np.zeros(self.arms)
+        if self.is_stationary:
+            self.q_true = np.random.randn(self.arms)
+        else:
+            self.q_true = np.zeros(self.arms) + self.q_true_initial
+        self.q_estimated = np.zeros(self.arms) + self.q_estimated_initial
         self.action_counts = np.zeros(self.arms)
+        self.t = 0
 
     def act(self):
         best_true_q = np.max(self.q_true)
         best_estimated_q = np.max(self.q_estimated)
-        if np.random.rand() < self.epsilon:
-            action = np.random.choice(self.actions)
+        if self.is_ucb:
+            action = 1
+        elif self.is_grad:
+            action = 1
         else:
-            action = np.random.choice(np.where(self.q_estimated == best_estimated_q)[0])
+            if np.random.rand() < self.epsilon:
+                action = np.random.choice(self.actions)
+            else:
+                action = np.random.choice(np.where(self.q_estimated == best_estimated_q)[0])
         self.action_counts[action] += 1
         is_best_action = self.q_true[action] == best_true_q
         return action, is_best_action
 
     def step(self, action):
         self.t += 1
+        if self.is_stationary:
+            self.q_true += np.random.normal(0, 0.01)
         reward = self.q_true[action] + np.random.randn()
         self.action_counts[action] += 1
         if self.is_sample_avg:
-            self.lr = 1 / self.t
+            self.lr = 1 / self.action_counts[action]
         self.q_estimated[action] += self.lr * (reward - self.q_estimated[action])
         return reward
 
 
-def run_simulation(bandits, runs=2000, time=1000):
+def run_simulation(bandits, runs, time):
     rewards = np.zeros((len(bandits), runs, time))
     best_actions = np.zeros(rewards.shape)
     for b, bandit in enumerate(bandits):
@@ -59,25 +70,55 @@ def run_simulation(bandits, runs=2000, time=1000):
     return rewards, best_actions
 
 
-def sec_2_3():
+def section_2_3():
     epsilons = [0.0, 0.01, 0.1]
     bandits = [Bandit(epsilon=epsilon, is_sample_avg=True) for epsilon in epsilons]
-    rewards, best_actions = run_simulation(bandits)
+    rewards, best_actions = run_simulation(bandits, runs=2000, time=1000)
+
     plt.figure(figsize=(10, 20))
     plt.subplot(2, 1, 1)
+
     for epsilon, reward in zip(epsilons, rewards):
         plt.plot(reward, label=epsilon)
         plt.xlabel('time')
         plt.ylabel('rewards')
         plt.legend()
     plt.subplot(2, 1, 2)
+
     for epsilon, best_action in zip(epsilons, best_actions):
         plt.plot(best_action, label=epsilon)
         plt.xlabel('time')
         plt.ylabel('% best actions')
         plt.legend()
+
     plt.savefig('../images/sec_2_3.png')
 
 
+def exercise_2_5():
+    is_sample_average = [True, False]
+    bandits = [Bandit(arms=20, epsilon=0.1, lr=0.1, is_stationary=False, is_sample_avg=is_avg)
+               for is_avg in is_sample_average]
+    rewards, best_actions = run_simulation(bandits, runs=1000, time=1000)
+
+    plt.figure(figsize=(10, 20))
+    plt.subplot(2, 1, 1)
+    labels = ['sample_avg', 'fixed_step']
+
+    for r, l in zip(rewards, labels):
+        plt.plot(r, label=l)
+    plt.xlabel('reward')
+    plt.ylabel('time')
+    plt.legend()
+    plt.subplot(2, 1, 2)
+
+    for p, l in zip(rewards, labels):
+        plt.plot(p, label=l)
+    plt.xlabel('% optimal action')
+    plt.ylabel('time')
+    plt.legend()
+
+    plt.savefig('../images/ex_2_5')
+
+
 if __name__ == '__main__':
-    sec_2_3()
+    exercise_2_5()

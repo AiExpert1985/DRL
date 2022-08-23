954ac1b77675a1104b7203a6f241b761adc7c4cd
Mohamad Nawfal <aiexpert2025@gmail.com>
2022-08-22 23:44:17 -0700

    ...

--- Book Sutton/chapter05/my_blackjack_mc_without_es.py
+++ Book Sutton/chapter05/my_blackjack_mc_without_es.py
@@ -145,7 +145,7 @@ def test(num_games):
 
 def run_simulation(total_games):
     train(total_games)
-    result = test(1000000)
+    result = test(100000)
     print("player's mean score of last 100 games = ", np.round(result, 2))
     print_order_states()
 
@@ -159,5 +159,5 @@ def print_order_states():
 
 
 if __name__ == '__main__':
-    n_games = 1000000
+    n_games = 10000000
     run_simulation(n_games)

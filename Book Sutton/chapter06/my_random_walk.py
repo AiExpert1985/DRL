fec93bf3e79c618f52419e0ec2282aaf48009be6
Mohamad Nawfal <aiexpert2025@gmail.com>
2022-09-04 02:39:00 -0700

    ...

--- Book Sutton/chapter06/my_random_walk.py
+++ Book Sutton/chapter06/my_random_walk.py
@@ -1,13 +1,19 @@
 import numpy as np
 from tqdm import tqdm
+from collections import defaultdict
 
 STATES = ['LEFT', 'A', 'B', 'C', 'D', 'E', 'RIGHT']
-INITIAL_VALUE = {state: 0.5 for state in STATES[1:6]}
-FINAL_VALUE = {state: (i+1)/6 for i, state in enumerate(STATES[1:6])}
+TRUE_VALUE = {state: (i+1)/6 for i, state in enumerate(STATES[1:6])}
+TERMINAL_STATE = ['LEFT', 'RIGHT']
 
 LEFT = -1
 RIGHT = 1
 ACTIONS = [LEFT, RIGHT]
+
+ALPHA = 0.1
+GAMMA = 1.0
+
+V = defaultdict(lambda: 0.5)
 
 
 def reward_fn(state_a, state_b):
@@ -27,27 +33,55 @@ def transition(state):
 def run_episode():
     rewards = 0
     state = 'C'
+    trajectory = []
     while state != 'RIGHT' and state != 'LEFT':
         next_state, reward = transition(state)
         rewards += reward
+        trajectory.append((state, next_state, reward))
         state = next_state
-    return rewards
+    return trajectory, rewards
+
+
+def temporal_difference(trajectory):
+    trajectory.reverse()
+    for state, next_state, reward in trajectory:
+        target = reward if next_state in TERMINAL_STATE else reward + GAMMA * V[next_state]
+        # print(state, next_state, reward)
+        # print('target =', target)
+        td_error = target - V[state]
+        # print('td_error =', td_error)
+        # print('before V[s] =', V[state])
+        V[state] += ALPHA * td_error
+        # print('after V[s] =', V[state])
+        # print("------------")
+
+def rmse_error():
+    se = []
+    for state in STATES[1: 6]:
+        e = V[state] - TRUE_VALUE[state]
+        se.append(np.square(e))
+    mse = np.mean(se)
+    rmse = np.sqrt(mse)
+    return rmse
+
+
+def monte_carlo(trajectory):
+    pass
 
 
 def run():
-    episodes = 1000000
+    episodes = 100
     rewards = 0
     for _ in tqdm(range(episodes)):
-        rewards += run_episode()
-    print(f'average rewards of {episodes} episode = {round(rewards/episodes, 2)}')
-
-
-def temporal_difference():
-    pass
-
-
-def monte_carlo():
-    pass
+        trajectory, reward = run_episode()
+        rewards += reward
+        temporal_difference(trajectory)
+        # monte_carlo(trajectory)
+    for state, val in V.items():
+        print(state, round(val, 2), round(TRUE_VALUE[state], 2))
+    error = rmse_error()
+    print('error =', round(error, 2))
+    # print(f'average rewards of {episodes} episode = {round(rewards/episodes, 2)}')
 
 
 if __name__ == '__main__':
